e1a143d80f19f92bf74013dacf2b3b09b10247d4
Sebastien Riou <sebastien.riou@pqshield.com>
2026-01-02 17:57:30 +0100

    build for embedded devices

---
+++ libpqcrystals-mldsa-lowram/source/config.h
@@ -0,0 +1,30 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//#define DILITHIUM_MODE 2
+// #define DILITHIUM_RANDOMIZED_SIGNING
+//#define USE_RDPMC
+//#define DBENCH
+
+#ifndef MLDSA_PSET
+#define MLDSA_PSET 44
+#endif
+
+#if MLDSA_PSET == 44
+#define DILITHIUM_MODE 2
+#define CRYPTO_ALGNAME "Dilithium2"
+#define DILITHIUM_NAMESPACETOP pqcrystals_dilithium2_lowram
+#define DILITHIUM_NAMESPACE(s) pqcrystals_dilithium2_lowram_##s
+#elif  MLDSA_PSET == 65
+#define DILITHIUM_MODE 3
+#define CRYPTO_ALGNAME "Dilithium3"
+#define DILITHIUM_NAMESPACETOP pqcrystals_dilithium3_lowram
+#define DILITHIUM_NAMESPACE(s) pqcrystals_dilithium3_lowram_##s
+#elif  MLDSA_PSET == 87
+#define DILITHIUM_MODE 5
+#define CRYPTO_ALGNAME "Dilithium5"
+#define DILITHIUM_NAMESPACETOP pqcrystals_dilithium5_lowram
+#define DILITHIUM_NAMESPACE(s) pqcrystals_dilithium5_lowram_##s
+#endif
+
+#endif
---
+++ libpqcrystals-mldsa-lowram/target/rv32imcb.cmake
@@ -0,0 +1,10 @@
+
+include("${CMAKE_CURRENT_LIST_DIR}/gcc-riscv-none-elf.cmake")
+set(CMAKE_SYSTEM_PROCESSOR riscv32)
+
+# Core MCU flags, CPU, instruction set and FPU setup
+set(cpu_PARAMS ${cpu_PARAMS}
+  -mabi=ilp32 
+  -march=rv32imc_zbb_zbc_zbs_zicsr
+)
+#_zbkb_zbkc_zbkx not supported by Renode
